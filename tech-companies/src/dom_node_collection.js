cd540de63210756b9c35f48268576a9044cb1cb3
guanw88 <guanw88@gmail.com>
2018-11-29 14:43:08 -0800

    Implement html and empty functions on DOMNodeCollection class

--- tech-companies/src/dom_node_collection.js
+++ tech-companies/src/dom_node_collection.js
@@ -2,6 +2,24 @@ class DOMNodeCollection {
   constructor (htmlElements) {
     this.htmlElements = htmlElements;
   }
+  
+  html (string) {
+    if (string === undefined) {
+      return this.htmlElements[0].innerHTML();
+    } else {
+      this.htmlElements[0].innerHTML = string;
+    }
+  }
+  
+  empty () {
+    for (let i = 0; i < this.htmlElements.length; i++) {
+      this.htmlElements[i].html("");
+    }
+  }
+  
+  append () {
+    
+  }
 }
 
 module.exports = DOMNodeCollection;
